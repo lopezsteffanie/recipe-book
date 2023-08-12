e10a55b9b848c35b9b387a0b57bb4b1253b733ac
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-08-12 12:29:32 -0400

    Add changing theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import Searchbar from './Searchbar';
+import { useTheme } from '../hooks/useTheme';
 
 export default function Navbar() {
+    const { color, changeColor } = useTheme();
+
     return (
-        <div className="navbar">
-            <nav>
+        <div className="navbar" style={{ background: color }}>
+            <nav onClick={() => changeColor('pink')}>
                 <Link to="/" className="brand">
                     <h1>Recipe Book</h1>
                 </Link>
